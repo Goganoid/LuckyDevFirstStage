bbd98a4b113883b8c70aef007547b6092db97440
Lucky4rever <snigur.pavlo@lll.kpi.ua>
2023-02-01 19:03:07 +0200

    fix 1

--- LuckyDev/ClientApp/src/components/Header.tsx
+++ LuckyDev/ClientApp/src/components/Header.tsx
@@ -21,7 +21,7 @@ const StyledHeader = styled(Navbar)`
   font-size: 1.5rem;
   padding: 1rem 1.5rem;
   background-color: #343A40;
-  heigth: 120px;
+  heigth: 60px;
 
   @media all and (max-width: 769px) {
     font-size: 1.5rem;
