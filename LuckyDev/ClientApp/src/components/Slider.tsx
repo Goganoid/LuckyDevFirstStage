43e5dc667e42d44f151ca29cdb9dab69513bd884
Goganoid <goganoid87@gmail.com>
2023-02-02 13:02:36 +0200

    Add slider caching

--- LuckyDev/ClientApp/src/components/Slider.tsx
+++ LuckyDev/ClientApp/src/components/Slider.tsx
@@ -3,16 +3,22 @@ import Carousel from 'react-bootstrap/Carousel';
 import { MealDbApi, type Meal } from 'src/api/mealdb.service';
 import styled from 'styled-components';
 import Button from 'react-bootstrap/Button';
+import dateDiffInDays from 'src/utils/dateDiff';
 
 const SliderImage = styled.img`
     display: block;
     width: 100%;
     height: 100%;
     object-fit: cover;
-    /* height:400px; */
 `
 const ItemStyle = {
-    height:'500px'
+    height: '500px'
+}
+
+
+type SliderStorage = {
+    items: Meal[],
+    last_update: string
 }
 
 
@@ -20,22 +26,42 @@ const Slider: FunctionComponent = () => {
 
     const [meals, setMeals] = useState<Meal[]>([]);
     useEffect(() => {
+        const stored_slider_str: string | null = window.localStorage.getItem('slider_items');
+        console.log(stored_slider_str);
+        if (stored_slider_str != null) {
+            const stored_slider = JSON.parse(stored_slider_str) as SliderStorage;
+            const daysSinceUpdate = dateDiffInDays(new Date(stored_slider.last_update), new Date());
+            console.log(daysSinceUpdate);
+            if (daysSinceUpdate < 7) {
+                setMeals(stored_slider.items);
+                return;
+            }
+        }
+
         MealDbApi.getRandomSelection().then(result => {
             console.log(result);
-            setMeals(result)
+            setMeals(result);
+            var date = new Date();
+            date.setDate(date.getDate() - 10);
+            var storage_item: SliderStorage = {
+                items: result,
+                last_update: date.toString()
+            }
+            window.localStorage.setItem('slider_items', JSON.stringify(storage_item))
         })
-    },[]);
+
+    }, []);
     return (
         <>
             <div className='d-flex justify-content-center'>
                 <Carousel className='w-50 h-100 button-on-slick'>
                     {meals.map((meal, idx) => (
                         <Carousel.Item key={idx} style={ItemStyle}>
-                        <SliderImage
-                            src={meal.strMealThumb}
-                            alt=""
-                        />
-                        <Carousel.Caption>
+                            <SliderImage
+                                src={meal.strMealThumb}
+                                alt=""
+                            />
+                            <Carousel.Caption>
                                 <h3>{meal.strMeal}</h3>
                                 <Button className='Bootstrap-Button' variant="primary">Скуштувати!</Button>
                         </Carousel.Caption>
