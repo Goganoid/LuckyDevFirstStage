d4d1706c0e9fcb3f49d4c50c785d06ad503856eb
Lucky4rever <snigur.pavlo@lll.kpi.ua>
2023-02-03 12:30:10 +0200

    Changes for popups

--- LuckyDev/ClientApp/src/components/Cards.tsx
+++ LuckyDev/ClientApp/src/components/Cards.tsx
@@ -6,10 +6,6 @@ import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import { MealsLoader } from 'src/api/meals_loader.service';
 
-const SliderImage = styled.img`
-    display: block;
-    height: 180px;
-`;
 
 const ItemWrapper = styled.div`
      flex: 30%;
@@ -64,16 +60,6 @@ const RecipeName = styled.span`
 `;
 
 const PopupYtLink = styled.a`
-`;
-
-const ModalFirstDiv = styled.div`
-    display: flex;
-    gap: 20px;
-`;
-
-const ModalSecondDiv = styled.div`
-    display: flex;
-    gap: 0 20px;
 `;
 
 const FilterMenu = styled.div`
@@ -160,7 +146,8 @@ const Cards: FunctionComponent = () => {
     let ingradientList: string[] = [];
     const findIngradients = () => {
         for (let i = 1; i <= 20; i++) {
-            ingradientList.push(`strIngredient${i}`)
+            if (`strMeasure${i}` !== '')
+                ingradientList.push(`strIngredient${i}`)
         }
     }
     findIngradients();
@@ -168,7 +155,8 @@ const Cards: FunctionComponent = () => {
     let measureList: string[] = [];
     const findMeasure = () => {
         for (let i = 1; i <= 20; i++) {
-            measureList.push(`strMeasure${i}`)
+            if (`strMeasure${i}` !== '')
+                measureList.push(`strMeasure${i}`)
         }
     }
     findMeasure();
@@ -192,9 +180,10 @@ const Cards: FunctionComponent = () => {
         <>
         <ItemWrapper>
             <Item key={idx} className='meal'>
-                <SliderImage
+                <img
                     src={m.strMealThumb}
                     alt=""
+                    height={180}
                 />
                 <RecipeContent>
                     <RecipeName>{m.strMeal}</RecipeName>
@@ -218,44 +207,44 @@ const Cards: FunctionComponent = () => {
             <div className="d-flex flex-column justify-content-center" id='left'>
                 <MealsList>
                 {meal.length ? meal : <h3>No Matches</h3>}
-                    <Modal show={show} onHide={handleClose} size="lg">
+                    <Modal show={show} onHide={handleClose} size="lg" className='popup'>
                         <Modal.Header closeButton>
                             <Modal.Title>{curMeal?.strMeal}</Modal.Title>
                         </Modal.Header>
-                        <Modal.Body className="show-grid">
-                            <ModalFirstDiv>
-                                <SliderImage src={curMealImg} alt="" />
-                                <div>
-                                    <p>Category: {curMeal?.strCategory || 'none'}</p>
-                                    <p>DrinkAlternate: {curMeal?.strDrinkAlternate || 'none'}</p>
-                                    <p>Area: {curMeal?.strArea || 'none'}</p>
-                                    <p>Tags: {curMeal?.strTags || 'none'}</p>
-                                    <p>Youtube: <PopupYtLink href={curMealLink}>{curMealLink || 'none'}</PopupYtLink></p>
+                        <Modal.Body className='show-grid'>
+                            <div className='modal-div'>
+                                <img src={curMealImg} alt="" className='popup-image'/>
+                                <div style={{width: 'calc(100% - 300px)'}}>
+                                    <div className='info'><span>Category: {curMeal?.strCategory || 'none'}</span></div>
+                                    <div className='info'><span>DrinkAlternate: {curMeal?.strDrinkAlternate || 'none'}</span></div>
+                                    <div className='info'><span>Area: {curMeal?.strArea || 'none'}</span></div>
+                                    <div className='info'><span>Tags: {curMeal?.strTags || 'none'}</span></div>
+                                    <div className='info' style={{overflow: 'hidden'}}><span>Youtube: <PopupYtLink href={curMealLink}>{curMealLink || 'none'}</PopupYtLink></span></div>
                                 </div>
-                            </ModalFirstDiv>
+                            </div>
                             <h3>Ingredients</h3>
-                            <ModalSecondDiv>
+                            <div className='modal-div info'>
                                 <div>
                                     {ingradientList.map((ingradient: string) => (
-                                        <p>{curMeal?.[ingradient] || ''}</p>
+                                        <span>{curMeal?.[ingradient] || ''}</span>
                                     ))}
                                 </div>
                                 <div>
                                     {measureList.map((measure: string) => (
-                                        <p>{curMeal?.[measure] || ''}</p>
+                                        <span>{curMeal?.[measure] || ''}</span>
                                     ))}
                                 </div>
-                            </ModalSecondDiv>
+                            </div>
                             <h3>Instruction</h3>
-                            <div>
-                                <p>{curMeal?.strInstructions}</p>
+                            <div className='info'>
+                                <span>{curMeal?.strInstructions}</span>
                             </div>
                         </Modal.Body>
                         <Modal.Footer>
-                            <Button variant="secondary" onClick={handleClose}>
+                            <Button variant="secondary" onClick={handleClose} className='Bootstrap-Button-white'>
                                 Close
                             </Button>
-                            <Button variant="primary" onClick={handleClose}>
+                            <Button variant="primary" onClick={handleClose} className='Bootstrap-Button'>
                                 Save Changes
                             </Button>
                         </Modal.Footer>
