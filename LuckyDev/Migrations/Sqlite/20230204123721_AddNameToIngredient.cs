28b1374a1be20ac56008bc893fceee2534ced637
Goganoid <goganoid87@gmail.com>
2023-02-04 14:52:55 +0200

    Added name to ingredient

---
+++ LuckyDev/Migrations/Sqlite/20230204123721_AddNameToIngredient.cs
@@ -0,0 +1,28 @@
+﻿#nullable disable
+
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RecipeWiki.Migrations.Sqlite
+{
+    /// <inheritdoc />
+    public partial class AddNameToIngredient : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "IngredientInfoId",
+                table: "Ingredients",
+                newName: "Name");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "Ingredients",
+                newName: "IngredientInfoId");
+        }
+    }
+}
